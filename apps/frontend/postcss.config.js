091d0fca6dd130facfe3a2edbc657110089bdf4f
Simanthi <j.simanthi@gmail.com>
2025-10-17 17:24:57 +0530

    commit

---
+++ apps/frontend/postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [
+    require('postcss-modules')({
+      generateScopedName: '[name]__[local]___[hash:base64:5]',
+      exportGlobals: true,
+      localsConvention: 'camelCase',
+    }),
+  ],
+};
